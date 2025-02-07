2dbca4eb39aa0c7bda29bd3df8c8a55eb7efcd14
Mojkan <elvinhillstrom@gmail.com>
2025-02-07 12:43:05 +0100

    Fixed errormessage

--- Assets/Scripts/ErrorText.cs
+++ Assets/Scripts/ErrorText.cs
@@ -14,7 +14,7 @@ public class ErrorText : MonoBehaviour
         RectTransform recTransform = gameObject.GetComponent<RectTransform>();
         recTransform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform);
         recTransform.localScale = Vector3.one;
-        recTransform.localPosition = new Vector3(0, yPosition, 0);
+        recTransform.anchoredPosition = new Vector3(0, yPosition, 0);
 
         errorText.text = "Error: " + errorMessage;
         Invoke(nameof(SelfDestruct), duration);
